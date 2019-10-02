01d20fdaff21c59938195220b16f3bf0e8cb5525
Samb102 <sam.benais@gmail.com>
2019-10-02 22:41:42 +0200

    m

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-require('offline-plugin/runtime').install();
-
-import './fonts/libre-baskerville-v5-latin-regular.woff';
-import './fonts/libre-baskerville-v5-latin-regular.woff2';
-
 import './index.html';
 import './index.scss';
 import './scripts/script.js';
